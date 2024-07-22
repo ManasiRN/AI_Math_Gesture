b83002906909e19692a1f820c552d4bd1ad9e483
Manasi Netrekar <162596385+ManasiRN@users.noreply.github.com>
2024-07-22 21:41:33 +0530

    main.py

--- mainn.py
+++ mainn.py
@@ -18,7 +18,7 @@ with col2:
     st.title("Answer")
     output_text_area = st.empty()
 
-genai.configure(api_key="AIzaSyCFAWzAXN7ABcwHeg4ekPbTTlHnWDBGaAQ")  # Replace with your actual API key
+genai.configure(api_key="Your API key")  # Replace with your actual API key
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Initialize the webcam to capture video
